3af870980cbcef279b4904c60463bbddef4141d9
Simon <mrsandman138@gmail.com>
2018-08-06 13:51:56 +0300

    Update
    Create method for creating Typescript file and for finding method types and its arguments. Ubiquity of Multimap. Enums are hard to add.

--- annotationProcessor/src/com/company/MyProcessor.java
+++ annotationProcessor/src/com/company/MyProcessor.java
@@ -7,7 +7,6 @@ import javafx.util.Pair;
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.*;
-import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.ElementFilter;
 import java.io.IOException;
@@ -32,60 +31,21 @@ public class MyProcessor extends AbstractProcessor {
                 TypeElement clazz = (TypeElement)annotatedElement;
                 String className = clazz.getSimpleName().toString();
 
-                Multimap<String, String> primitiveNames = ArrayListMultimap.create();
-                findPrimitiveTypes(clazz, primitiveNames);
+                Multimap<String, String> variableNames = ArrayListMultimap.create();
+                findVariableTypes(clazz, variableNames);
 
-                Map<String, TypeMirror> methodReturnTypes = new HashMap<>();
-                Map<String, List<Pair<String, TypeMirror>>> funcArguments = new HashMap<>();
+                Multimap<TypeMirror, String> methodReturnTypes = ArrayListMultimap.create();
+                Multimap<String, Pair<TypeMirror, String>> funcArguments = ArrayListMultimap.create();
+                findMethodTypes(clazz, methodReturnTypes, funcArguments);
 
-                for (ExecutableElement method
-                        : ElementFilter.methodsIn(clazz.getEnclosedElements())) {
-                    TypeMirror methodType = method.getReturnType();
-                    String methodName = method.getSimpleName().toString();
-                    methodReturnTypes.put(methodName, methodType);
-                    funcArguments.put(methodName, new ArrayList<>());
-                    for (VariableElement arg : method.getParameters()) {
-                        TypeMirror argType = arg.asType();
-                        String argName = arg.getSimpleName().toString();
-                        funcArguments.get(methodName).add(new Pair<>(argName, argType));
-                    }
-                }
-                try {
-                    PrintWriter pw = new PrintWriter(className + ".ts", "UTF-8");
-                    pw.println("interface I" + className + " {");
-                    for (Map.Entry<String, String> name : primitiveNames.entries()) {
-                        pw.println("    " + name.getValue() + " : " + name.getKey());
-                    }
-                    for (Map.Entry<String, TypeMirror> fun : methodReturnTypes.entrySet()){
-                        String funcName = fun.getKey();
-                        pw.print("    " + funcName + "(");
-                        List<Pair<String, TypeMirror>> argList = funcArguments.get(fun.getKey());
-                        if (argList != null) {
-                            int size = argList.size();
-                            for (Pair<String, TypeMirror> arg : argList) {
-                                pw.print(arg.getKey() + ": ");
-                                String convertedArg = convertJavaToTypeScriptType(arg.getValue());
-                                pw.print(convertedArg);
-                                size--;
-                                if (size > 0) pw.print(", ");
-                            }
-                        }
-                        String convertedFun = convertJavaToTypeScriptType(fun.getValue());
-                        pw.println(") : " + convertedFun);
-                    }
-                    pw.println("}");
-                    pw.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                printInterface(className, variableNames, methodReturnTypes, funcArguments);
             }
         }
         return true;
     }
 
-    private String convertJavaToTypeScriptType(TypeMirror clazz) {
+    private String convertPrimitiveTypes(TypeMirror clazz) {
         String variableType = clazz.getKind().toString();
-        System.out.println(variableType);
         switch (variableType){
             case "CHAR":
                 return "string";
@@ -106,24 +66,68 @@ public class MyProcessor extends AbstractProcessor {
             case "VOID":
                 return "void";
         }
-        if (clazz.toString().equals((String.class.getCanonicalName()))) return "string";
+        if (clazz.toString().equals(String.class.getTypeName())) return "string";
         return "";
     }
 
-    private void findPrimitiveTypes(TypeElement clazz, Multimap<String, String> primitiveTypes){
+
+    private void findVariableTypes(TypeElement clazz, Multimap<String, String> types){
         for (VariableElement field
                 : ElementFilter.fieldsIn(clazz.getEnclosedElements())) {
-            System.out.println(String.class.getSimpleName());
             TypeMirror fieldClass = field.asType();
             String fieldName = field.getSimpleName().toString();
-            String convertedType = convertJavaToTypeScriptType(fieldClass);
-            primitiveTypes.put(convertedType, fieldName);
+            String convertedPrimType = convertPrimitiveTypes(fieldClass);
+            types.put(convertedPrimType, fieldName);
         }
     }
 
-    /*private void printInterface(String classname){
+    private void findMethodTypes(TypeElement clazz, Multimap<TypeMirror, String> methodtypes, Multimap<String, Pair<TypeMirror, String>> funcarg){
+        for (ExecutableElement method
+                : ElementFilter.methodsIn(clazz.getEnclosedElements())) {
+            TypeMirror methodType = method.getReturnType();
+            String methodName = method.getSimpleName().toString();
+            methodtypes.put(methodType, methodName);
+            for (VariableElement arg : method.getParameters()) {
+                TypeMirror argType = arg.asType();
+                String argName = arg.getSimpleName().toString();
+                funcarg.put(methodName, new Pair<>(argType, argName));
+            }
+        }
+    }
 
-    }*/
+    private void printInterface(String className,
+                                Multimap<String, String> variableNames,
+                                Multimap<TypeMirror, String> methodReturnTypes,
+                                Multimap<String, Pair<TypeMirror, String>> funcArguments){
+        try {
+            PrintWriter pw = new PrintWriter(className + ".ts", "UTF-8");
+            pw.println("interface I" + className + " {");
+            for (Map.Entry<String, String> name : variableNames.entries()) {
+                pw.println("    " + name.getValue() + " : " + name.getKey());
+            }
+            for (Map.Entry<TypeMirror, String> fun : methodReturnTypes.entries()){
+                String funcName = fun.getValue();
+                pw.print("    " + funcName + "(");
+                Collection<Pair<TypeMirror, String>> argList = funcArguments.get(fun.getValue());
+                if (argList != null) {
+                    int size = argList.size();
+                    for (Pair<TypeMirror, String> arg : argList) {
+                        pw.print(arg.getValue() + ": ");
+                        String convertedArg = convertPrimitiveTypes(arg.getKey());
+                        pw.print(convertedArg);
+                        size--;
+                        if (size > 0) pw.print(", ");
+                    }
+                }
+                String convertedFun = convertPrimitiveTypes(fun.getKey());
+                pw.println(") : " + convertedFun);
+            }
+            pw.println("}");
+            pw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public Set<String> getSupportedAnnotationTypes() {
